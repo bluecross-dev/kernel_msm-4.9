277270f85f949d90e588d3c7621054d23c60970b
Artem Labazov <123321artyom@gmail.com>
2019-06-14 15:12:33 +0300

    Revert "Revert "sched/fair: Turn off FBT_STRICT_ORDER feature""
    This reverts commit 1c0482dd38cfb8540cd2a4ff44be0685853e7f3e.
    
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -97,7 +97,7 @@ SCHED_FEAT(MIN_CAPACITY_CAPPING, false)
  * ON: If the target CPU saves any energy, use that.
  * OFF: Use whichever of target or backup saves most.
  */
-SCHED_FEAT(FBT_STRICT_ORDER, true)
+SCHED_FEAT(FBT_STRICT_ORDER, false)
 /*
  * Enforce schedtune.prefer_idle to take need_idle path.
  * ON: schedtune.prefer_idle is replaced with need_idle
