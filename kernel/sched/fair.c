43074e8d3a2978fef238996ac6f581bd7c74f882
Artem Labazov <123321artyom@gmail.com>
2019-06-14 15:24:14 +0300

    Revert "ANDROID: sched/fair: unify spare capacity calculation"
    This reverts commit 66c5bae9af058007be34b78e4bdef88ea5632e95.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7047,7 +7047,6 @@ retry:
 			unsigned long capacity_curr = capacity_curr_of(i);
 			unsigned long capacity_orig = capacity_orig_of(i);
 			unsigned long wake_util, new_util;
-			long spare_cap;
 
 			cpumask_clear_cpu(i, &search_cpus);
 
@@ -7079,13 +7078,6 @@ retry:
 			new_util = max(min_util, new_util);
 			if (new_util > capacity_orig)
 				continue;
-
-			/*
-			 * Pre-compute the maximum possible capacity we expect
-			 * to have available on this CPU once the task is
-			 * enqueued here.
-			 */
-			spare_cap = capacity_orig - new_util;
 
 			/*
 			 * Case A) Latency sensitive tasks
@@ -7138,9 +7130,9 @@ retry:
 				 * Case A.2: Target ACTIVE CPU
 				 * Favor CPUs with max spare capacity.
 				 */
-				if (capacity_curr > new_util &&
-				    spare_cap > target_max_spare_cap) {
-					target_max_spare_cap = spare_cap;
+				if ((capacity_curr > new_util) &&
+					(capacity_orig - new_util > target_max_spare_cap)) {
+					target_max_spare_cap = capacity_orig - new_util;
 					target_cpu = i;
 					continue;
 				}
@@ -7246,10 +7238,10 @@ retry:
 
 			/* Favor CPUs with maximum spare capacity */
 			if (capacity_orig == target_capacity &&
-			    spare_cap < target_max_spare_cap)
+			    (capacity_orig - new_util) < target_max_spare_cap)
 				continue;
 
-			target_max_spare_cap = spare_cap;
+			target_max_spare_cap = capacity_orig - new_util;
 			target_capacity = capacity_orig;
 			target_util = new_util;
 			target_cpu = i;
